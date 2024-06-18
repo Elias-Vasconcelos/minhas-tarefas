0b11be4d41f4b5368ae24c614524be83adfbc94a
Elias-Vasconcelos <146135801+Elias-Vasconcelos@users.noreply.github.com>
2024-06-18 13:42:40 -0300

    Finalização do componente Tarefas

--- src/conponents/Tarefas/index.tsx
+++ src/conponents/Tarefas/index.tsx
@@ -1,14 +1,30 @@
 import * as S from './styles' 
 
+type Props = {
+    titulo: string
+    prioridade: string
+    status: string
+    descricao: string
+    }
+    
 const Tarefas = () => (
 <S.Card>
- <S.Titulo> Nome da tarefa </S.Titulo>
- <S.Tag>importante</S.Tag>
-  <S.Tag>pendente</S.Tag> 
-  <S.Descricao />
+ <S.Titulo>{titulo}</S.Titulo>
+ <S.Tag>{prioridade}</S.Tag>
+  <S.Tag>{status}</S.Tag> 
+  <S.Descricao value={descricao} />
  <S.BarraAcoes>
-  <S.Botao>Editar</S.Botao>
-  <S.Botao>Remover</S.Botao> 
+    { estaEditando ? (
+        <>
+          <S.Botao>Salvar</S.Botao>
+          <S.Botao>Cancelar</S.Botao> 
+        </>
+    ) : (
+        <>
+          <S.Botao>Editar</S.Botao>
+          <S.Botao>Remover</S.Botao> 
+        </>
+    ) }
  </S.BarraAcoes>
 </S.Card>
 )
--- src/conponents/Tarefas/styles.ts
+++ src/conponents/Tarefas/styles.ts
@@ -4,6 +4,7 @@ export const Card = styled. div`
  background-color: #fcfcfc;
  box-shadow: Opx 4px 4px rgba(0, 0, 0, 0.25);
  padding: 16px;
+ margin-botton: 16px;
 `
 
 export const Titulo = styled.h3`
@@ -13,12 +14,19 @@ export const Titulo = styled.h3`
 export const Tag = styled.span`
  padding: 4px 8px;
  color: #fff;
- font-weight: bold;
- font-size: 10px;
+ font-weight: bold; font-size: 10px;
+ background-color: #ela32a;
+ border-radius: 8px; margin-right: 16px; display: inline-block;
 `
 export const Descricao = styled.textarea`
  color: #8b8b8b; font-size: 14px; line-height: 24px;
  font-family: 'Roboto Mono', monospace;
+ display: block;
+ width: 100%;
+ margin-bottom: 16px; margin-top: 16px;
+ resize: none;
+ Pborder: none;
+ background-color: transparent;
 `
 
 export const BarraAcoes = styled.div`
